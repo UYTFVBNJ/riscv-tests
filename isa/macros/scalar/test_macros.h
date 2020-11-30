757dbe3b79ea4649c8d4d6dca954a7f74affcb7e
UYTFVBNJ <191250034@smail.nju.edu.cn>
2020-11-30 19:53:31 +0800

    transplant: make rv32ui

--- isa/macros/scalar/test_macros.h
+++ isa/macros/scalar/test_macros.h
@@ -706,11 +706,10 @@ test_ ## testnum: \
 #-----------------------------------------------------------------------
 
 #define TEST_PASSFAIL \
-        bne x0, TESTNUM, pass; \
 fail: \
         RVTEST_FAIL; \
 pass: \
-        RVTEST_PASS \
+        RVTEST_PASS; \
 
 
 #-----------------------------------------------------------------------
